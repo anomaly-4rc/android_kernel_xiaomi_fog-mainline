502e3872e25f681fd4cec44fa2b721de4ec93764
anomaly-4rc <miquu.official@gmail.com>
2026-07-23 17:27:36 +0700

    fix: increase drawobj burst size and dispatcher inflight limits

--- drivers/gpu/msm/adreno_dispatch.c
+++ drivers/gpu/msm/adreno_dispatch.c
@@ -20,7 +20,7 @@ static unsigned int _context_drawqueue_size = 64;
 static unsigned int _context_queue_wait = 10000;
 
 /* Number of drawobjs sent at a time from a single context */
-static unsigned int _context_drawobj_burst = 5;
+static unsigned int _context_drawobj_burst = 128;
 
 /*
  * GFT throttle parameters. If GFT recovered more than
@@ -35,13 +35,13 @@ static unsigned int _fault_throttle_burst = 3;
  * Maximum ringbuffer inflight for the single submitting context case - this
  * should be sufficiently high to keep the GPU loaded
  */
-static unsigned int _dispatcher_q_inflight_hi = 20;
+static unsigned int _dispatcher_q_inflight_hi = 128;
 
 /*
  * Minimum inflight for the multiple context case - this should sufficiently low
  * to allow for lower latency context switching
  */
-static unsigned int _dispatcher_q_inflight_lo = 8;
+static unsigned int _dispatcher_q_inflight_lo = 64;
 
 /* Command batch timeout (in milliseconds) */
 unsigned int adreno_drawobj_timeout = 2000;
